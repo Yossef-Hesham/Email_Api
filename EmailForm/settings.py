beb8b7262b6a62f2a80f8df04d2a15bfddfd023f
youssef <youss4923@gmail.com>
2025-03-06 03:09:58 +0200

    first commit

--- EmailForm/settings.py
+++ EmailForm/settings.py
@@ -131,8 +131,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'youss4923@gmail.com'
-EMAIL_HOST_PASSWORD = 'qcaxqcpgsmdzmvgb'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
